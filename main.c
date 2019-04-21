cc718688c3ff4a7a4917ddd8fcab9f3dad0d5784
JayKempton <49607367+JayKempton@users.noreply.github.com>
2019-04-21 17:58:05 +1000

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,32 @@
 
 int main() {
 
+char message[100], ch;  //use the array message to store the user input
+int x, key;
+printf("Enter a message to encrypt: ");
+gets(message);
+printf("Enter key: ");
+scanf("%d", &key);
+for(x = 0; message[x] != '\0'; ++x){    //use a for loop to count 
+ch = message[x];
+if(ch >= 'a' && ch <= 'z'){
+ch = ch + key;
+if(ch > 'z'){
+ch = ch - 'z' + 'a' - 1;
+}
+message[x] = ch;
+}
+else if(ch >= 'A' && ch <= 'Z'){
+ch = ch + key;
+if(ch > 'Z'){
+ch = ch - 'Z' + 'A' - 1;
+}
+message[x] = ch;
+}
+}
+printf("Encrypted message: %s", message);
+return 0;
+
 
 return (0)
 }
